bf8e12394f74f33c4e92856c9bf139e10db9449f
Mateus Pereira <madpprofile@gmail.com>
2017-04-06 17:58:35 -0300

    Initial commit

---
+++ pyramid.py
@@ -0,0 +1,78 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+vertices = (
+    ( 1, 0, -1),
+    ( -1, 0, -1),
+    (-1, 0, 1),
+    (1, 0, 1),
+    (0, 1, 0),
+    )
+
+linhas = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    )
+
+faces = (
+    (0,1,4),
+    (1,2,4),
+    (2,3,4),
+    (3,0,4),
+    (0,1,2),
+    (2,3,0),
+    )
+
+cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+
+def Cubo():
+    glBegin(GL_TRIANGLE_STRIP)
+    i = 0
+    for face in faces:
+#        glColor3fv(cores[i])
+        for vertex in face:
+            glColor3fv(cores[vertex])
+            glVertex3fv(vertices[vertex])
+        i = i+1
+    glEnd()
+
+    glColor3fv((0,0.5,0))
+    glBegin(GL_LINES)
+    for linha in linhas:
+        for vertice in linha:
+            glVertex3fv(vertices[vertice])
+    glEnd()
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glRotatef(2,1,3,0)
+    Cubo()
+    glutSwapBuffers()
+ 
+def timer(i):
+    glutPostRedisplay()
+    glutTimerFunc(50,timer,1)
+
+# PROGRAMA PRINCIPAL
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
+glutInitWindowSize(800,600)
+glutCreateWindow("CUBO")
+glutDisplayFunc(display)
+glEnable(GL_MULTISAMPLE)
+glEnable(GL_DEPTH_TEST)
+glClearColor(0.,0.,0.,1.)
+gluPerspective(45,800.0/600.0,0.1,50.0)
+glTranslatef(0.0,0.0,-10)
+glRotatef(0,0,0,0)
+glutTimerFunc(50,timer,1)
+glutMainLoop()
+
+
